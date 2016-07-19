d13c544834a68de60e4b2182032c148855d557b0
Vladimir Suhov <vladimir_suhov@outlook.com>
2016-07-19 18:36:13 +0300

    Update CompareMinNoAsc.cs

--- ASP.NET.1607.Day2.Task2.Suhov.Matrix/CompareMinNoAsc.cs
+++ ASP.NET.1607.Day2.Task2.Suhov.Matrix/CompareMinNoAsc.cs
@@ -13,8 +13,13 @@ namespace ASP.NET._1607.Day2.Task2.Suhov.Matrix
         private int _iy;
         // maintain a reference to the 2-dimensional array being sorted
         private int[][] sortArray;
-        // constructor initializes the sortArray reference
+    /// <summary> 
+    /// Default constructor, initialize variables with default values 
+    /// </summary> 
         public CompareMinNoAsc() { }
+     /// <summary> 
+     /// Constructor with parameters, initialize variables with the parameters value 
+     /// </summary> 
         public CompareMinNoAsc(int[][] theArray)
         {
             sortArray = theArray;
@@ -26,8 +31,8 @@ namespace ASP.NET._1607.Day2.Task2.Suhov.Matrix
         {
             if (x == null || y == null || !(x is int) || !(y is int)) throw new ArgumentException(String.Format("{0}, {1} is not an int values", x, y));
             if (sortArray[_ix] == null) throw new ArgumentException(String.Format("{0} has no values inside, please run constructor with parameters", sortArray[_ix]));
-            _ix = (int)x;
-            _iy = (int)y;
+                _ix = (int)x;
+                _iy = (int)y;
             if (sortArray[_ix].Min() >= sortArray[_iy].Min())
                 return 1;
             else
