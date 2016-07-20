5e16f78d760f643e0baa4e61fa1d0a4aee023bdb
vladimir_suhov <vladimir_suhov@outlook.com>
2016-07-20 04:56:33 -0700

    refactoring V02

--- ASP.NET.1607.Day2.Task2.Suhov.Matrix.Console/Program.cs
+++ ASP.NET.1607.Day2.Task2.Suhov.Matrix.Console/Program.cs
@@ -13,25 +13,36 @@ namespace ASP.NET._1607.Day2.Task2.Suhov.Matrix.Console
         {
             Matrix matrix = new Matrix(4,5);
             int[][] matrixValues=matrix.GetMatrix;
-            System.Console.WriteLine(matrix.GetMatrixString);
+            System.Console.WriteLine("Original matrix");
+            System.Console.WriteLine(matrix.ToString());
+            matrixValues[0][0] = 11111111;
+            System.Console.WriteLine("Changed value from property");
+            System.Console.WriteLine(matrix.ToString());
+            matrix.Sort(Matrix.CompareMaxNoAsc);
+            System.Console.WriteLine("Sorted matrix with defined sorting logic");
+            System.Console.WriteLine(matrix.ToString());
+
+            #region Old implementation
+            System.Console.WriteLine(matrix.ToString());
             IComparer compare = new CompareMaxNoAsc(matrix.GetMatrix);
             matrix.Sort(compare);
-            System.Console.WriteLine(matrix.GetMatrixString);
+            System.Console.WriteLine(matrix.ToString());
             compare = new CompareMaxNoDesc(matrix.GetMatrix);
             matrix.Sort(compare);
-            System.Console.WriteLine(matrix.GetMatrixString);
+            System.Console.WriteLine(matrix.ToString());
             compare = new CompareMinNoAsc(matrix.GetMatrix);
             matrix.Sort(compare);
-            System.Console.WriteLine(matrix.GetMatrixString);
+            System.Console.WriteLine(matrix.ToString());
             compare = new CompareMinNoDesc(matrix.GetMatrix);
             matrix.Sort(compare);
-            System.Console.WriteLine(matrix.GetMatrixString);
+            System.Console.WriteLine(matrix.ToString());
             compare = new CompareSumNoAsc(matrix.GetMatrix);
             matrix.Sort(compare);
-            System.Console.WriteLine(matrix.GetMatrixString);
+            System.Console.WriteLine(matrix.ToString());
             compare = new CompareSumNoDesc(matrix.GetMatrix);
             matrix.Sort(compare);
-            System.Console.WriteLine(matrix.GetMatrixString);
+            System.Console.WriteLine(matrix.ToString());
+            #endregion
             System.Console.ReadLine();
         }
     }
--- ASP.NET.1607.Day2.Task2.Suhov.Matrix/Matrix.cs
+++ ASP.NET.1607.Day2.Task2.Suhov.Matrix/Matrix.cs
@@ -10,36 +10,69 @@ namespace ASP.NET._1607.Day2.Task2.Suhov.Matrix
     public class Matrix
     {
         #region Private fields
-        private int _rowNum;
-        private int _colNum;
         private int[][] _matrix;
         private int[] _matrixSubArray;
         #endregion
-    /// <summary>
-    /// Return jagged array with matrix inside
-    /// </summary>
-        public int[][] GetMatrix { get { return _matrix; } }
-    /// <summary>
-    /// Return jagged array as a string
-    /// </summary>
-        public string GetMatrixString { get { return GetOutputStringArray(); } }
-    /// <summary>
-    /// Default constructor, initialize variables with default values
-    /// </summary>
+        /// <summary>
+        /// Return multidimentional array with matrix inside
+        /// </summary>
+        public int[][] GetMatrix { get { return CopyArray(); } }
+        /// <summary>
+        /// Return multidimentional array as a string
+        /// </summary>
+        public override string ToString()
+        {
+            return GetOutputStringArray();
+        }
+        /// <summary>
+        /// Default constructor, initialize variables with default values
+        /// </summary>
         public Matrix() { }
-    /// <summary>
-    /// Constructor with parameters, initialize variables with the parameters value
-    /// </summary>
+        /// <summary>
+        /// Constructor with parameters, initialize variables with the parameters value
+        /// </summary>
         public Matrix(int rowNum, int colNum)
         {
-            _rowNum = rowNum;
-            _colNum = colNum;
-            FillMatrix();
+            FillMatrix(rowNum, colNum);
             FillSubArray();
         }
-    /// <summary>
-    /// Sort matrix with required comparison
-    /// </summary>
+        /// <summary>
+        /// Delegate which used for sorting method to pass two arrays inside
+        /// </summary>
+        public delegate int CompareRows(int[] array1, int[] array2);
+        /// <summary>
+        /// Sort matrix with required comparer
+        /// </summary>
+        public void Sort(CompareRows Compare)
+        {
+            if (_matrix == null)
+                throw new ArgumentNullException(nameof(_matrix));
+            if (Compare == null)
+                throw new ArgumentNullException(nameof(Compare));
+            for (int i = _matrix.Length - 1; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (Compare(_matrix[j], _matrix[j + 1]) > 0)
+                    {
+                        Swap(ref _matrix[j], ref _matrix[j + 1]);
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// The comparer method for sorting delegate
+        /// </summary>
+        public static int CompareMaxNoAsc(int[] array1, int[] array2)
+        {
+            if (array1.Max() >= array2.Max())
+                return 1;
+            else
+                return -1;
+        }
+        /// <summary>
+        /// Sort matrix with required comparison
+        /// </summary>
         public void Sort(IComparer comparison)
         {
             Array.Sort(_matrixSubArray, comparison);
@@ -51,20 +84,20 @@ namespace ASP.NET._1607.Day2.Task2.Suhov.Matrix
         #region Private methods
         private void FillSubArray()
         {
-            _matrixSubArray = new int[_rowNum];
-            for (int i = 0; i < _rowNum; i++)
+            _matrixSubArray = new int[_matrix.Length];
+            for (int i = 0; i < _matrix.Length; i++)
             {
                 _matrixSubArray[i] = i;
             }
         }
-        private void FillMatrix()
+        private void FillMatrix(int rowNum, int colNum)
         {
             var num = new System.Random();
-            _matrix = new int[_rowNum][];
-            for (int i = 0; i < _rowNum; i++)
+            _matrix = new int[rowNum][];
+            for (int i = 0; i < rowNum; i++)
             {
-                int[] item = new int[_colNum];
-                for (int ii = 0; ii < _colNum; ii++)
+                int[] item = new int[colNum];
+                for (int ii = 0; ii < colNum; ii++)
                 {
                     item[ii] = num.Next(1, 10);
                 }
@@ -83,6 +116,26 @@ namespace ASP.NET._1607.Day2.Task2.Suhov.Matrix
                 result.Append("\r\n");
             }
             return result.ToString();
+        }
+        private int[][] CopyArray()
+        {
+            int[][] result=new int[_matrix.Length][];
+            for (int i = 0; i < _matrix.Length; i++)
+            {
+                int[] item = new int[_matrix[i].Length];
+                for (int ii = 0; ii < _matrix[i].Length; ii++)
+                {
+                    item[ii] = _matrix[i][ii];
+                }
+                result[i] = item;
+            }
+            return result;
+        }
+        private void Swap(ref int[] firstArray, ref int[] secondArray)
+        {
+            int[] bufferArray = firstArray;
+            firstArray = secondArray;
+            secondArray = bufferArray;
         }
         #endregion
 
